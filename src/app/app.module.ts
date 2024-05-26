565a895b250e098c28b8f51fbee6028eef6765bc
Timoté LANCELLE <timote.lancelle@outlook.fr>
2024-05-26 22:56:38 +0200

    Ajout prettier, mise à jour des fichiers nouvelles versions angular, ajout header, mise à jour css globale normes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, NotFoundComponent],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Component, OnInit } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { OlympicService } from "src/app/core/services/olympic.service";
+import { HeaderComponent } from "../../components/header/header.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"],
+    standalone: true,
+    imports: [CommonModule, HeaderComponent],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+    public olympics$: Observable<any> = of(null);
 
-  constructor(private olympicService: OlympicService) {}
+    constructor(private olympicService: OlympicService) {}
 
-  ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-  }
+    ngOnInit(): void {
+        this.olympics$ = this.olympicService.getOlympics();
+    }
 }

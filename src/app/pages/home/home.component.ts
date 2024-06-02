4e98d05ffd7e7b0b03be8486135ebdc61556f5c3
Timoté LANCELLE <timote.lancelle@outlook.fr>
2024-06-02 23:10:42 +0200

    Mise en forme css pages et composants

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { Observable, of } from "rxjs";
 import { OlympicService } from "src/app/core/services/olympic.service";
 import { HeaderComponent } from "../../components/header/header.component";
 import { CommonModule } from "@angular/common";
+import { FooterComponent } from "src/app/components/footer/footer.component";
 
 @Component({
     selector: "app-home",
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.scss"],
     standalone: true,
-    imports: [CommonModule, HeaderComponent],
+    imports: [CommonModule, HeaderComponent, FooterComponent],
 })
 export class HomeComponent implements OnInit {
     public olympics$: Observable<any> = of(null);
